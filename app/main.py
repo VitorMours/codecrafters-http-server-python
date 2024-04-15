b22ec9402f2cbce4015a7d3b8018de96de1b574f
João Vítor Rezende Moura <jvrezendemoura@gmail.com>
2024-04-15 13:31:56 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ class WebServer:
         self.port = port 
         self.server = None
         self.paths = ['']
-        
+        self.request_data = None
+
+    def get_user_agent(self):
+        return self.request_data['User-Agent']
 
     def create_server(self, not_run:bool) -> socket.socket:
         self.server = socket.create_server((self.host, self.port), reuse_port=True)
@@ -27,11 +30,12 @@ class WebServer:
         match http_path:
             case s if http_path.startswith("/echo/"):
                 http_path = http_path.lstrip("/echo/")
-                _str = "HTTP/1.1 200\r\n"
-                _str += "Content-Type: text/plain\r\n" 
-                _str += f"Content-Length:{len(http_path)} \r\n\r\n"
+                _str = self.create_response(http_path)
+            
+            case s if http_path.startswith("/user-agent"):
+                _str = self.create_response(self.get_user_agent())
+                
 
-                _str += f"{http_path}\r\n\r\n"
 
             case "/":
                 _str = "HTTP/1.1 200 OK\r\n\r\n"
@@ -58,8 +62,6 @@ class WebServer:
             if ": " in info:
                 key, value = info.split(": ", 1)
                 _dict[key] = value 
-            else:
-                print(f"Ignorando linha mal formatada: {info}")
         
         if return_dict:
             return _dict
@@ -70,75 +72,29 @@ class WebServer:
         return _str
 
 
-    def create_response(self, status:int) -> None:
-        pass
+    def create_response(self, data:str) -> str:
+        _str = 'HTTP/1.1 200\r\n'
+        _str += "Content-Type: text/plain\r\n"
+        _str += f"Content-Length:{len(data)}\r\n\r\n"
+        _str += f"{data}\r\n\r\n"
+
+        return _str
 
     def run(self, server: socket.socket=None) -> None:
         
         if self.server is None:
-            self.server = self.create_server()
+            server = self.create_server()
         
         print("Logs from your program will appear here")
         while True:
             socket, address = self.server.accept()
              
-            data = socket.recvmsg(1024)
-            data = self._clean_request(data, return_dict=True)
+            self.request_data = socket.recvmsg(1024)
+            self.request_data = self._clean_request(self.request_data, return_dict=True)
+            self.show_request(self.request_data)
+            socket.send(self.http_code(self.request_data["Request"]).encode())
 
-            socket.send(self.http_code(data["Request"]).encode())
-
-#            socket.send(response)
-
-
-
-
-
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-   
-    server_socket = socket.create_server(("localhost", 4221), reuse_port = True)
-    print(f"\n\n{type(server_socket)}\n\n")
-    while True:
-        sock, addr = server_socket.accept() # wait for client
-        print(sock, addr)
-        
-        # Pegando os dados recebidos, e tentando enetneder eles de maneira mais organizada
-        data = sock.recvmsg(1024)
-        message = data[0]
-        print(f"{' Message Data ':=^80}\n{data}")
-        print("\n" + message.decode())
-        print(bytes(message))
-        
-        
-        http_message = (message.decode()).split("\r\n")[0:3]
-#        print(http_message)
-       
-        http_path = (http_message[0].split())[1]
-#        print(http_path)
-#        print(http_path.encode())
-        status_message = "HTTP/1.1 200 OK\r\n"
-            
-        
-            
-
-        
-        if http_path.startswith("/echo/"):
-            http_path = http_path.strip()
-#            print(http_path)
-            status_message += 'Content-Type: text/plain\r\n'
-            path_message = http_path.lstrip('/echo/')
-            status_message += f'Content-Length: {len(path_message)}\r\n'
-            status_message += f'\r\n\r\n{path_message}'
-
-
-
-        print(status_message)
-        sock.send(status_message.encode())
 
 if __name__ == "__main__":
     webserver = WebServer()
     webserver.create_server(not_run=False)
-    #main()
